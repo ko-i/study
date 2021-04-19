eb38122ad05331249fe685a094c8a302e455531d
Akson <Akson.L@outlook.com>
2021-04-19 14:31:17 +0800

    update in 2021-04-19 14:31:16

---
+++ mastering-rust/ch05/refcell_basics.rs
@@ -0,0 +1,32 @@
+use std::cell::RefCell;
+
+#[derive(Debug)]
+struct Bag {
+    item: Box<u32>,
+}
+
+fn main() {
+    let bag = RefCell::new(
+        Bag {
+            item: Box::new(1),
+        }
+    );
+
+    let hand1 = &bag;
+    let hand2 = &bag;
+
+    *hand1.borrow_mut() = Bag {
+        item: Box::new(2),
+    };
+
+    *hand2.borrow_mut() = Bag {
+        item: Box::new(3),
+    };
+
+    let borrowed = hand1.borrow();
+    println!("{:?}", borrowed);
+
+    // println!("{:?} {:?}", hand1.borrow(), hand1.borrow_mut());
+    // println!("{:?}", hand1.borrow_mut());
+    // println!("{:?}", hand1.borrow_mut());
+}
