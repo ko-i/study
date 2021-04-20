34294a9879779fc9421fd06f249ffe3dfc6acc1f
Akson <Akson.L@outlook.com>
2021-04-20 21:57:29 +0800

    update in 2021-04-20 21:57:29

---
+++ mastering-rust/ch07/type_alias.rs
@@ -0,0 +1,16 @@
+pub struct ParsedPayload<T> {
+    inner: T
+}
+
+pub struct ParseError<E> {
+    inner: E
+}
+
+type ParserResult<T, E> = Result<ParsedPayload<T>, ParseError<E>>;
+pub fn parse_payload<T, E>(stream: &[u8]) -> ParserResult<T, E> {
+    unimplemented!();
+}
+
+fn main() {
+    // todo
+}
