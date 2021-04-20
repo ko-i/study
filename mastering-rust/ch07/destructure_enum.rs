969f57efc177d4795c4ed8fe4c9f05c0d6a22061
Akson <Akson.L@outlook.com>
2021-04-20 21:02:25 +0800

    update in 2021-04-20 21:02:24

---
+++ mastering-rust/ch07/destructure_enum.rs
@@ -0,0 +1,13 @@
+enum Container {
+    Item(u64),
+    Empty,
+}
+
+fn main() {
+    let maybe_item = Container::Item(0u64);
+    let has_item = if let Container::Item(0) = maybe_item {
+        true
+    } else {
+        false
+    };
+}
---
+++ mastering-rust/ch07/refutable_pattern.rs
@@ -0,0 +1,9 @@
+enum Container {
+    Item(u64),
+    Empty
+}
+
+fn main() {
+    let mut item = Container::Item(56);
+    let Container::Item(it) = item;
+}
