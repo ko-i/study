3063061646e8cf123cd2fcd0d25c99a7f8081404
Akson <Akson.L@outlook.com>
2022-02-27 20:17:10 +0800

    update in 2022-02-27 20:17:10

---
+++ core-java-volume-i-fundamentals/ch05/arrays/CopyOfTest.java
@@ -0,0 +1,40 @@
+package arrays;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class CopyOfTest {
+    
+    public static void main(String[] args) {
+        
+        int[] a = {1, 2, 3};
+        a = (int[]) goodCopyOf(a, 10);
+        System.out.println(Arrays.toString(a));
+        
+        String[] b = {"T", "D", "H"};
+        b = (String[]) goodCopyOf(b, 10);
+        System.out.println(Arrays.toString(b));
+    }
+
+    public static Object[] badCopyOf(Object[] a, int newLength) {
+        Object[] newArray = new Object[newLength];
+        System.arraycopy(a, 0, newArray, 0, Math.min(a.length, newLength));
+
+        return newArray;
+    }
+
+    public static Object goodCopyOf(Object a, int newLength) {
+        Class cl = a.getClass();
+
+        if (!cl.isArray()) return null;
+
+        Class componentType = cl.getComponentType();
+
+        int length = Array.getLength(a);
+        Object newArray = Array.newInstance(componentType, newLength);
+
+        System.arraycopy(a, 0, newArray, 0, Math.min(length, newLength));
+
+        return newArray;
+    }
+}
