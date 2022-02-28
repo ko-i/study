27dfadec4427e00038e14ee6decb11cbc04a9e06
Akson <Akson.L@outlook.com>
2022-02-28 11:22:43 +0800

    update in 2022-02-28 11:22:43

---
+++ core-java-volume-i-fundamentals/ch06/timer/TimerTest.java
@@ -0,0 +1,32 @@
+package timer;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Date;
+
+import javax.swing.JOptionPane;
+import javax.swing.Timer;
+
+public class TimerTest {
+
+    public static void main(String[] args) {
+        
+        ActionListener listener = new TimePrinter();
+
+        Timer t = new Timer(3000, listener);
+
+        t.start();
+
+        JOptionPane.showMessageDialog(null, "Quit program?");
+        System.exit(0);
+    }
+    
+}
+
+class TimePrinter implements ActionListener {
+
+    public void actionPerformed(ActionEvent event) {
+
+        System.out.println("At the tone, the time is " + new Date());
+    }
+}
